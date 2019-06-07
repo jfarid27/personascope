72c150af8155bf07528e7a75feeeb0b943041808
J. A. Farid <jrab227@gmail.com>
2019-06-07 00:02:54 -0400

    updates

--- src/components/InsightGraph/index.spec.js
+++ src/components/InsightGraph/index.spec.js
@@ -1,4 +1,5 @@
-import { generateNodes } from '.';
+import { find } from 'lodash';
+import { generateNodes, generateLinks } from '.';
 
 describe('Insight Graph', () => {
   describe('GenerateNodes', () => {
@@ -10,11 +11,43 @@ describe('Insight Graph', () => {
         { Action: 'bar', SessionID: 2 },
       ];
       const result = generateNodes(rowData);
+      const expected = [
+        { id: 'foo', counts: 1 },
+        { id: 'bar', counts: 2 },
+        { id: 'cat', counts: 1 },
+      ];
       it('should generate a node list with the right ids', () => {
         const ids = result.map(i => i.id);
-        const expected = ['foo', 'bar', 'cat'];
-        expected.map(id => expect(ids).toContain(id));
-        expect(ids.length).toBe(3);
+        const matchingId = node => n => n.id === node.id;
+        expected.map(node => expect(ids).toContain(node.id));
+        expected.map(node => expect(find(result, matchingId(node)).counts).toBe(node.counts));
+        expect(result.length).toBe(3);
+      });
+    });
+  });
+  describe('GenerateLinks', () => {
+    describe('when given rowData', () => {
+      const rowData = [
+        { Action: 'foo', SessionID: 1 },
+        { Action: 'bar', SessionID: 1 },
+        { Action: 'cat', SessionID: 2 },
+        { Action: 'bar', SessionID: 2 },
+        { Action: 'cat', SessionID: 3 },
+        { Action: 'bar', SessionID: 3 },
+      ];
+      const result = generateLinks(rowData);
+      const expected = [
+        { source: 'foo', target: 'bar', counts: 1 },
+        { source: 'cat', target: 'bar', counts: 2 },
+      ];
+      it('should generate a node list with the right ids', () => {
+        const matchingEdge = edge => n => (n.source === edge.source && n.target === edge.target);
+        expected.map(
+          edge => expect(
+            find(result, matchingEdge(edge)).counts,
+          ).toBe(edge.counts),
+        );
+        expect(result.length).toBe(2);
       });
     });
   });
